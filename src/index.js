13bb8a7563a12c089c3082cc11e36158fd528d6f
ngactive99 <ngactive99@gmail.com>
2022-04-30 21:11:34 +0530

    removed app.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+ <h1>Welcome to react redux session</h1>
   </React.StrictMode>
 );
 
