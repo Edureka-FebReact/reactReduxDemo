f3b0754ab9137d21e09cd171c2f2b62321a57ef6
ngactive99 <ngactive99@gmail.com>
2022-04-30 21:25:35 +0530

    created final file structure and the store

---
+++ src/containers/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1> redux demo </h1>
+            </div>
+        )
+    }
+}
+export default App
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware } from "redux";
+
+let store = createStore(applyMiddleware())
+
+export default store;
